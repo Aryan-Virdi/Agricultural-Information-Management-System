f551d4a72b0601cfb76b2b4b31e6b1331d4f9bf3
Aryan Virdi <avirdi2@ucmerced.edu>
2025-11-19 22:24:49 -0800

    Fixed Q1 to include pH stats again. Rounded to nearest hundredth.

--- main/sql_script/queries.sql
+++ main/sql_script/queries.sql
@@ -7,42 +7,48 @@
 .print "==========================================="
 .print "QUERY 1: Soil Component Statistics by Field"
 .print "==========================================="
+
 WITH 
 mean_components AS (
-    SELECT 
-        ss_fieldkey AS fieldkey, 
-        AVG(ss_sand) AS mean_sand, 
-        AVG(ss_silt) AS mean_silt, 
-        AVG(ss_clay) AS mean_clay, 
-        COUNT(*) AS n 
-    FROM soilsample
-    GROUP BY ss_fieldkey
+  SELECT 
+    ss_fieldkey AS fieldkey, 
+    ROUND(AVG(ss_sand), 2) AS mean_sand, 
+    ROUND(AVG(ss_silt), 2) AS mean_silt, 
+    ROUND(AVG(ss_clay), 2) AS mean_clay,
+    ROUND(AVG(ss_ph), 2)  AS mean_ph,
+    COUNT(*) AS n 
+  FROM soilsample
+  GROUP BY ss_fieldkey
 ),
 squared_diff AS (
-    SELECT mc.fieldkey, 
-        POWER((mc.mean_sand - sample.ss_sand), 2) AS sand, 
-        POWER((mc.mean_silt - sample.ss_silt), 2) AS silt, 
-        POWER((mc.mean_clay - sample.ss_clay), 2) AS clay 
-    FROM soilsample AS sample
-    JOIN mean_components AS mc ON sample.ss_fieldkey = mc.fieldkey
+  SELECT mc.fieldkey, 
+    POWER((mc.mean_sand - sample.ss_sand), 2) AS sand, 
+    POWER((mc.mean_silt - sample.ss_silt), 2) AS silt, 
+    POWER((mc.mean_clay - sample.ss_clay), 2) AS clay, 
+    POWER((mc.mean_ph - sample.ss_ph), 2) AS ph
+  FROM soilsample AS sample
+  JOIN mean_components AS mc ON sample.ss_fieldkey = mc.fieldkey
 ),
 variance AS (
-    SELECT sd.fieldkey, 
-        (SUM(sd.sand)/(mc.n - 1)) AS var_sand, 
-        (SUM(sd.silt)/(mc.n - 1)) AS var_silt, 
-        (SUM(sd.clay)/(mc.n - 1)) AS var_clay 
-    FROM squared_diff AS sd
-    JOIN mean_components AS mc ON sd.fieldkey = mc.fieldkey
-    GROUP BY sd.fieldkey
+  SELECT sd.fieldkey, 
+    (SUM(sd.sand)/(mc.n - 1)) AS var_sand, 
+    (SUM(sd.silt)/(mc.n - 1)) AS var_silt, 
+    (SUM(sd.clay)/(mc.n - 1)) AS var_clay,
+    (SUM(sd.ph)/(mc.n-1)) AS var_ph
+  FROM squared_diff AS sd
+  JOIN mean_components AS mc ON sd.fieldkey = mc.fieldkey
+  GROUP BY sd.fieldkey
 )
 SELECT 
     v.fieldkey,
     mean_sand,
+    ROUND(SQRT(var_sand), 2) AS std_dev_sand,
     mean_silt,
+    ROUND(SQRT(var_silt), 2) AS std_dev_silt, 
     mean_clay,
-    SQRT(var_sand) AS std_dev_sand, 
-    SQRT(var_silt) AS std_dev_silt, 
-    SQRT(var_clay) AS std_dev_clay 
+    ROUND(SQRT(var_clay), 2) AS std_dev_clay,
+    mean_ph,
+    ROUND(SQRT(var_ph), 2) AS std_dev_ph
 FROM variance v
 JOIN mean_components AS mc ON v.fieldkey = mc.fieldkey
 ORDER BY v.fieldkey;
